09e18a6e115570a6aab24f87be0b35728249ea22
slava <FBI13377@yandex.ru>
2023-12-22 03:52:00 +0300

    Создал сетку и поменял цвет фона

--- app/src/main/java/com/example/battle_tanks_1_2/MainActivity.kt
+++ app/src/main/java/com/example/battle_tanks_1_2/MainActivity.kt
@@ -7,20 +7,49 @@ import android.view.KeyEvent.KEYCODE_DPAD_DOWN
 import android.view.KeyEvent.KEYCODE_DPAD_LEFT
 import android.view.KeyEvent.KEYCODE_DPAD_RIGHT
 import android.view.KeyEvent.KEYCODE_DPAD_UP
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.FrameLayout
+import androidx.core.view.marginLeft
+import androidx.core.view.marginTop
 import com.example.battle_tanks_1_2.Direction.UP
 import com.example.battle_tanks_1_2.Direction.DOWN
 import com.example.battle_tanks_1_2.Direction.LEFT
 import com.example.battle_tanks_1_2.Direction.RIGHT
 import com.example.battle_tanks_1_2.databinding.ActivityMainBinding
 
+const val  CELL_SIZE = 50
+
+lateinit var binding: ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private val gridDrawer by lazy {
+        GridDrawer(this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.title="Menu"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.settings, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_settings -> {
+                gridDrawer.drawGrid()
+                return true
+            }
+
+
+        else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
@@ -37,22 +66,30 @@ class MainActivity : AppCompatActivity() {
         when (direction){
             UP -> {
                 binding.myTank.rotation = 0f
-                (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin += -50
+                if (binding.myTank.marginTop > 0) {
+                    (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin += -CELL_SIZE
+                }
             }
 
             DOWN -> {
                 binding.myTank.rotation = 180f
-                (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin += 50
+                if (binding.myTank.marginTop + binding.myTank.height < binding.container.height / CELL_SIZE * CELL_SIZE) {
+                    (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin += CELL_SIZE
+                }
             }
 
             LEFT -> {
                 binding.myTank.rotation = 270f
-                (binding.myTank.layoutParams as FrameLayout.LayoutParams).leftMargin -= 50
+                if (binding.myTank.marginLeft > 0) {
+                    (binding.myTank.layoutParams as FrameLayout.LayoutParams).leftMargin -= CELL_SIZE
+                }
             }
 
             RIGHT -> {
                 binding.myTank.rotation = 90f
-                (binding.myTank.layoutParams as FrameLayout.LayoutParams).leftMargin += 50
+                if (binding.myTank.marginLeft + binding.myTank.width < binding.container.width / CELL_SIZE * CELL_SIZE) {
+                    (binding.myTank.layoutParams as FrameLayout.LayoutParams).leftMargin += CELL_SIZE
+                }
             }
         }
         binding.container.removeView(binding.myTank)
